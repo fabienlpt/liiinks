42f7ef18a6ec46a532a12a47194c35a780f7e8da
Alyssa <alychrf62300@gmail.com>
2024-05-02 17:04:38 +0200

    modify username ajout des autres clés

--- src/app/[username]/page.tsx
+++ src/app/[username]/page.tsx
@@ -7,6 +7,22 @@ import { useEffect, useState } from "react";
 interface User {
   username: string;
   email: string;
+  bio: string;
+  mainColor: string;
+  backgroundGradient: string;
+  fontChoice: string;
+  avatar: string;
+  twitter: string;
+  facebook : string;
+  linkedIn: string;
+  behance: string;
+  instagram: string;
+  firstLink: string;
+  secondLink: string;
+  thirdLink: string;
+  fourthLink: string;
+  fifthLink: string;
+
 }
 
 export default function Page({ params }: { params: { username: string } }) {
@@ -18,23 +34,70 @@ export default function Page({ params }: { params: { username: string } }) {
       axios.get(`/api/user?username=${params.username}`).then((response) => {
         if (response.data.length > 0) setUser(response.data);
         else setUser(null);
-      });
-      setLoading(false);
+
+        console.log(response.data);
+        setLoading(false);
+    });
     }
   }, [params.username]);
 
   return (
     <div className="flex min-h-screen flex-col items-center p-24">
-      <Header isConnected={false} />
+      <Header />
       {loading ? (
         <div>Loading...</div>
       ) : user ? (
         <div className="max-w-md w-full space-y-8">
-          <div>
-            <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
-              Bonjour {params.username} !
-            </h2>
-          </div>
+           <div>
+      <h1>Profil</h1>
+      {user.avatar && (
+        <img src={user.avatar} alt="Avatar" />
+      )}
+      {user.username && (
+        <p>Username: {user.username}</p>
+      )}
+      {user.bio && (
+        <p>Bio: {user.bio}</p>
+      )}
+      <div>
+        <h1>Réseaux sociaux</h1>
+        <ul>
+          {user.twitter && (
+            <li>Twitter: {user.twitter}</li>
+          )}
+          {user.facebook && (
+            <li>Facebook: {user.facebook}</li>
+          )}
+          {user.linkedIn && (
+            <li>LinkedIn: {user.linkedIn}</li>
+          )}
+          {user.behance && (
+            <li>Behance: {user.behance}</li>
+          )}
+          {user.instagram && (
+            <li>Instagram: {user.instagram}</li>
+          )}
+        </ul>
+        <h1>Autres Liens</h1>
+        <ul>
+          {user.firstLink && (
+            <li>First Link: {user.firstLink}</li>
+          )}
+          {user.secondLink && (
+            <li>Second Link: {user.secondLink}</li>
+          )}
+          {user.thirdLink && (
+            <li>Third Link: {user.thirdLink}</li>
+          )}
+          {user.fourthLink && (
+            <li>Fourth Link: {user.fourthLink}</li>
+          )}
+          {user.fifthLink && (
+            <li>Fifth Link: {user.fifthLink}</li>
+          )}
+        </ul>
+      </div>
+    </div>
         </div>
       ) : (
         <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
