3f96ec5d7cf66e65b2ce6b1ba6209ac23655cbaf
Fabien <lapertfabin@gmail.com>
2024-05-04 13:37:18 +0200

    Add avatar upload functionality and update user preferences

--- src/app/api/preferences/route.ts
+++ src/app/api/preferences/route.ts
@@ -1,3 +1,4 @@
+import { FieldSet } from "airtable";
 import { base } from "../config";
 import fs from "fs";
 import path from "path";
@@ -6,18 +7,18 @@ export async function POST(request: Request) {
   try {
     const data = await request.formData();
 
-    const userId = data.get("userId");
+    const userId = data.get("userId") as string;
     const mainColor = data.get("mainColor");
     const backgroundGradient = data.get("backgroundColor");
     const fontChoice = data.get("fontChoice");
-    const avatarFile = data.get("avatar");
+    const avatarFile = data.get("avatar") as File;
 
     const fieldsToUpdate = {
-      mainColor,
-      backgroundGradient,
-      fontChoice,
+      mainColor: mainColor ?? "",
+      backgroundGradient: backgroundGradient ?? "",
+      fontChoice: fontChoice ?? "",
       avatar: "",
-    };
+    } as FieldSet;
 
     if (avatarFile) {
       const avatarDir = path.join(process.cwd(), "public", "avatars");
@@ -43,6 +44,7 @@ export async function POST(request: Request) {
 
     const updatedUser = updatedRecords[0].fields;
 
+    console.log("Préférences mises à jour :", updatedUser);
     return new Response(JSON.stringify({ user: updatedUser }), {
       status: 200,
       headers: {
